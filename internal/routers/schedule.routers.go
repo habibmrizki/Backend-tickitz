505fb056ef0039e664312dad7432f987e8e5942f
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-08 10:11:17 +0700

    feat : add file schedule.routers.go

---
+++ internal/routers/schedule.routers.go
@@ -0,0 +1,20 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/habibmrizki/back-end-tickitz/internal/handlers"
+	"github.com/habibmrizki/back-end-tickitz/internal/repositories"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// InitScheduleRouter menginisialisasi rute-rute yang berkaitan dengan jadwal
+func InitScheduleRouter(router *gin.Engine, db *pgxpool.Pool) {
+	scheduleRepo := repositories.NewScheduleRepository(db)
+	scheduleHandler := handlers.NewScheduleHandlers(scheduleRepo)
+
+	scheduleGroup := router.Group("/schedules")
+	{
+		scheduleGroup.GET("", scheduleHandler.GetAllSchedules)
+		scheduleGroup.GET("/:id", scheduleHandler.GetScheduleByMovieId)
+	}
+}
