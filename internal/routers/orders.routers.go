92853035811a1659e6e9ca417ec0bc5e1c2c2814
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-08 10:07:25 +0700

    feat : add file orders.routers.go

---
+++ internal/routers/orders.routers.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/habibmrizki/back-end-tickitz/internal/handlers"
+	"github.com/habibmrizki/back-end-tickitz/internal/repositories"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// InitOrderRouter menginisialisasi rute-rute yang berkaitan dengan order
+func InitOrderRouter(router *gin.Engine, db *pgxpool.Pool) {
+	orderRepo := repositories.NewOrderRepository(db)
+	orderHandler := handlers.NewOrderHandlers(orderRepo)
+
+	ordersGroup := router.Group("/orders")
+	{
+		ordersGroup.POST("", orderHandler.CreateOrder)
+		ordersGroup.GET("/history/:userId", orderHandler.GetOrderHistory)
+		ordersGroup.GET("/:orderId", orderHandler.GetOrderByID)
+	}
+}
