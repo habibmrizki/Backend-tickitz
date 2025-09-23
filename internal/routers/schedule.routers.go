fd0fba7f43667b1e890b2b742277081520a76ed6
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-23 20:02:22 +0700

    feat : add middlewares access for user and admin

--- internal/routers/schedule.routers.go
+++ internal/routers/schedule.routers.go
@@ -3,16 +3,19 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/habibmrizki/back-end-tickitz/internal/handlers"
+	"github.com/habibmrizki/back-end-tickitz/internal/middlewares"
 	"github.com/habibmrizki/back-end-tickitz/internal/repositories"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
 // InitScheduleRouter menginisialisasi rute-rute yang berkaitan dengan jadwal
-func InitScheduleRouter(router *gin.Engine, db *pgxpool.Pool) {
+func InitScheduleRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	scheduleRepo := repositories.NewScheduleRepository(db)
 	scheduleHandler := handlers.NewScheduleHandlers(scheduleRepo)
 
-	scheduleGroup := router.Group("/schedules")
-	scheduleGroup.GET("", scheduleHandler.GetAllSchedules)
-	scheduleGroup.GET("/:id", scheduleHandler.GetScheduleByMovieId)
+	scheduleGroup := router.Group("/schedules").Use(middlewares.VerifyTokenWithBlacklist(rdb))
+	scheduleGroup.GET("", middlewares.Access("user"), scheduleHandler.GetAllSchedules)
+	scheduleGroup.GET("/movie/:movieId", middlewares.Access("user", "admin"), scheduleHandler.GetSchedulesByMovieID)
+	// scheduleGroup.GET("/:id", scheduleHandler.GetScheduleByMovieId)
 }
