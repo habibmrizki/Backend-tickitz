3d87c970f737412ae3634e014b6bdc6768199432
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-23 20:02:52 +0700

    feat : add redis to init router

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -8,17 +8,27 @@ import (
 	"github.com/habibmrizki/back-end-tickitz/internal/middlewares"
 	"github.com/habibmrizki/back-end-tickitz/internal/models"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func InitRouter(db *pgxpool.Pool) *gin.Engine {
+func InitRouter(db *pgxpool.Pool, rdb *redis.Client) *gin.Engine {
 	router := gin.Default()
 	router.Use(middlewares.MyLogger)
 	router.Use(middlewares.CORSMiddleware)
 
 	docs.SwaggerInfo.BasePath = "/"
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	router.Static("/img", "./public/images")
+	// Panggil fungsi inisialisasi rute-rute khusus
+	InitUsersRouter(router, db, rdb)
+	InitAdminMovieRouter(router, db, rdb)
+	InitMovieRouter(router, db, rdb)
+	InitScheduleRouter(router, db, rdb)
+	InitOrderRouter(router, db, rdb)
+	InitSeatRouter(router, db, rdb)
 
 	// Catch all route
 	router.NoRoute(func(ctx *gin.Context) {
@@ -28,11 +38,5 @@ func InitRouter(db *pgxpool.Pool) *gin.Engine {
 		})
 	})
 
-	// Panggil fungsi inisialisasi rute-rute khusus
-	InitUsersRouter(router, db)
-	InitMovieRouter(router, db)
-	InitScheduleRouter(router, db)
-	InitOrderRouter(router, db)
-	InitSeatRouter(router, db)
 	return router
 }
