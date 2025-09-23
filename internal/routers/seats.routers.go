1e11004f08fbdf8a2d239492e8f0178be5c998ca
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-23 20:03:18 +0700

    feat : add middlewares access for users

--- internal/routers/seats.routers.go
+++ internal/routers/seats.routers.go
@@ -3,16 +3,18 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/habibmrizki/back-end-tickitz/internal/handlers"
+	"github.com/habibmrizki/back-end-tickitz/internal/middlewares"
 	"github.com/habibmrizki/back-end-tickitz/internal/repositories"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
 // InitSeatRouter menginisialisasi rute-rute yang berkaitan dengan kursi
-func InitSeatRouter(router *gin.Engine, db *pgxpool.Pool) {
+func InitSeatRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	seatRepo := repositories.NewSeatRepository(db)
 	seatHandler := handlers.NewSeatHandler(seatRepo)
 
-	seatGroup := router.Group("/seats")
-	seatGroup.GET("/:scheduleId/available", seatHandler.GetAvailableSeats)
-	seatGroup.GET("/seat/:seatId", seatHandler.GetSeatByID)
+	seatGroup := router.Group("/seats").Use(middlewares.VerifyTokenWithBlacklist(rdb))
+	seatGroup.GET("/:scheduleId/available", middlewares.Access("user"), seatHandler.GetAvailableSeats)
+	// seatGroup.GET("/seat/:seatId", seatHandler.GetSeatByID)
 }
