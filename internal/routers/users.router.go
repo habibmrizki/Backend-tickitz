dff14fdfbdfa0d6bce5cfdd960216ef158d0705b
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-23 20:01:56 +0700

    feat " add several router users

--- internal/routers/users.router.go
+++ internal/routers/users.router.go
@@ -6,10 +6,11 @@ import (
 	"github.com/habibmrizki/back-end-tickitz/internal/middlewares"
 	"github.com/habibmrizki/back-end-tickitz/internal/repositories"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
 // InitUsersRouter menginisialisasi semua rute yang berkaitan dengan pengguna
-func InitUsersRouter(router *gin.Engine, db *pgxpool.Pool) {
+func InitUsersRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	usersGroup := router.Group("/auth")
 
 	// Inisialisasi Repositori dan Handler
@@ -21,18 +22,19 @@ func InitUsersRouter(router *gin.Engine, db *pgxpool.Pool) {
 	usersGroup.POST("/login", userHandler.UserLogin)
 
 	usersGroup.GET("/:id", userHandler.GetProfileById)
-	usersGroup.PUT("/:id", userHandler.UpdateProfile)
+	// usersGroup.PATCH("/:id", userHandler.UpdateProfile)
 
 	// Grup rute yang memerlukan otentikasi JWT.
 	// Middleware `VerifyToken` akan memeriksa token JWT dan menyimpan klaim (termasuk peran) di konteks.
-	apiGroup := router.Group("/users").Use(middlewares.VerifyToken)
+	apiGroup := router.Group("/users").Use(middlewares.VerifyTokenWithBlacklist(rdb))
 
-	// Rute ini dapat diakses oleh "user" dan "admin".
-	// Middleware `middlewares.Access("user", "admin")` memastikan hanya user dengan salah satu dari peran ini yang bisa mengakses.
-	apiGroup.GET("/:id", middlewares.Access("user", "admin"), userHandler.GetProfileById)
+	apiGroup.GET("", middlewares.Access("user"), userHandler.GetProfileById)
 
-	// Rute ini hanya dapat diakses oleh "admin".
-	// Middleware `middlewares.Access("admin")` membatasi akses hanya untuk admin.
-	apiGroup.PUT("/:id", middlewares.Access("admin"), userHandler.UpdateProfile)
+	// Middleware `middlewares.Access("user")` membatasi akses hanya untuk admin.
+	// apiGroup.PATCH("/:id", middlewares.Access("user"), userHandler.UpdateProfile)
+	apiGroup.PATCH("", middlewares.Access("user"), userHandler.UpdateProfile)
+
+	// apiGroup.GET("/dashboard", userHandler.GetDashboard)
+	apiGroup.POST("/logout", handlers.LogoutHandler(rdb))
 
 }
