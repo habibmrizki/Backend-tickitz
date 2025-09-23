4969b71a99da084196980053ba4dbafaa0def03a
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-23 20:17:18 +0700

    feat : command profile struct and add binding required in password

--- internal/models/users.models.go
+++ internal/models/users.models.go
@@ -3,15 +3,15 @@ package models
 type UsersStruct struct {
 	Id       int    `db:"id,omitempty" json:"id,omitempty"`
 	Email    string `db:"email" json:"email" form:"email" binding:"required,email"`
-	Password string `db:"password" json:"password" form:"password"`
+	Password string `db:"password" json:"password" form:"password" binding:"required"`
 	Role     string `db:"role,omitempty" json:"role,omitempty"`
 }
 
-type Profile struct {
-	User_Id      int    `db:"users_id" json:"users_id"`
-	Firstname    string `db:"first_name" json:"first_name"`
-	Lastname     string `db:"last_name" json:"last_name"`
-	PhoneNumber  string `db:"phone_number" json:"phone_number"`
-	ProfileImage string `db:"Profile_image" json:"Profile_image"`
-	Point        int    `db:"point" json:"point"`
-}
+// type Profile struct {
+// 	User_Id      int    `db:"users_id" json:"users_id"`
+// 	Firstname    string `db:"first_name" json:"first_name"`
+// 	Lastname     string `db:"last_name" json:"last_name"`
+// 	PhoneNumber  string `db:"phone_number" json:"phone_number"`
+// 	ProfileImage string `db:"Profile_image" json:"Profile_image"`
+// 	Point        int    `db:"point" json:"point"`
+// }
