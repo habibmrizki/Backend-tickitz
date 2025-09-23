8334f40462e4413cd2c8dcb4ec59eeb75265b7e5
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-23 20:15:13 +0700

    feat : add veryfy token blacklist

--- internal/middlewares/verifyToken.go
+++ internal/middlewares/verifyToken.go
@@ -61,7 +61,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/habibmrizki/back-end-tickitz/internal/configs"
 	"github.com/habibmrizki/back-end-tickitz/pkg"
+	"github.com/redis/go-redis/v9"
 )
 
 func VerifyToken(ctx *gin.Context) {
@@ -108,4 +110,66 @@ func VerifyToken(ctx *gin.Context) {
 	ctx.Set("claims", claims)
 	ctx.Set("role", claims.Role)
 	ctx.Next()
+}
+
+// Tambahkan fungsi ini di bawah fungsi VerifyToken yang sudah ada
+func VerifyTokenWithBlacklist(rdb *redis.Client) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		bearerToken := ctx.GetHeader("Authorization")
+		if bearerToken == "" || !strings.HasPrefix(bearerToken, "Bearer ") {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"success": false,
+				"error":   "Authorization header is missing or malformed",
+			})
+			return
+		}
+		token := strings.Split(bearerToken, " ")[1]
+
+		// Cek blacklist token di Redis
+		blacklisted, err := configs.IsTokenBlacklisted(rdb, token)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"success": false,
+				"error":   "Internal Server Error",
+			})
+			return
+		}
+		if blacklisted {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"success": false,
+				"error":   "Token sudah logout, silahkan login kembali",
+			})
+			return
+		}
+
+		// Lanjutkan dengan verifikasi token seperti kode Anda
+		var claims pkg.Claims
+		if err := claims.VerifyToken(token); err != nil {
+			if err == jwt.ErrTokenInvalidIssuer {
+				log.Println("JWT Error.\nCause: ", err.Error())
+				ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+					"success": false,
+					"error":   "Silahkan login kembali",
+				})
+				return
+			}
+			if err == jwt.ErrTokenExpired {
+				log.Println("JWT Error.\nCause: ", err.Error())
+				ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+					"success": false,
+					"error":   "Silahkan login kembali",
+				})
+				return
+			}
+			log.Println("Internal Server Error.\nCause: ", err.Error())
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"success": false,
+				"error":   "Internal Server Error",
+			})
+			return
+		}
+		ctx.Set("claims", claims)
+		ctx.Set("role", claims.Role)
+		ctx.Next()
+	}
 }
