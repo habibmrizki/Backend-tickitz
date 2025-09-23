af5335d7edb365b780473441cd89ab7bf9102e6a
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-23 20:03:49 +0700

    feat : add middleware for users and verify token

--- internal/routers/orders.routers.go
+++ internal/routers/orders.routers.go
@@ -3,17 +3,20 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/habibmrizki/back-end-tickitz/internal/handlers"
+	"github.com/habibmrizki/back-end-tickitz/internal/middlewares"
 	"github.com/habibmrizki/back-end-tickitz/internal/repositories"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
 // InitOrderRouter menginisialisasi rute-rute yang berkaitan dengan order
-func InitOrderRouter(router *gin.Engine, db *pgxpool.Pool) {
+func InitOrderRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	orderRepo := repositories.NewOrderRepository(db)
 	orderHandler := handlers.NewOrderHandlers(orderRepo)
 
-	ordersGroup := router.Group("/orders")
-	ordersGroup.POST("", orderHandler.CreateOrder)
-	ordersGroup.GET("/history/:userId", orderHandler.GetOrderHistory)
-	ordersGroup.GET("/:orderId", orderHandler.GetOrderByID)
+	ordersGroup := router.Group("/orders").Use(middlewares.VerifyTokenWithBlacklist(rdb))
+	ordersGroup.POST("", middlewares.VerifyToken, middlewares.Access("user"), orderHandler.CreateOrder)
+	ordersGroup.GET("/history/:userId", middlewares.VerifyToken, middlewares.Access("user"), orderHandler.GetOrderHistory)
+	ordersGroup.GET("/:orderId", middlewares.VerifyToken, middlewares.Access("user"), orderHandler.GetOrderByID)
+
 }
