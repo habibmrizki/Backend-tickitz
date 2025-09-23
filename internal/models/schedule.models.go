2d934231a12e73320f053171f55cffeda7cae3d3
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-09-23 20:18:08 +0700

    feat : change structure struct scheduledetails

--- internal/models/schedule.models.go
+++ internal/models/schedule.models.go
@@ -13,13 +13,13 @@ type Schedule struct {
 }
 
 // ScheduleDetails adalah model untuk respons yang lebih detail
-type ScheduleDetails struct {
-	ID         int       `json:"id"`
-	Date       time.Time `json:"date"`
-	CinemaName string    `json:"cinema_name"`
-	Location   string    `json:"location"`
-	Time       time.Time `json:"time"`
-}
+// type ScheduleDetails struct {
+// 	ID         int       `json:"id"`
+// 	Date       time.Time `json:"date"`
+// 	CinemaName string    `json:"cinema_name"`
+// 	Location   string    `json:"location"`
+// 	Time       time.Time `json:"time"`
+// }
 
 // ResponseSchedule adalah model respons API untuk daftar jadwal
 type ResponseSchedule struct {
@@ -33,4 +33,14 @@ type ResponseScheduleByMovie struct {
 	Status  string            `json:"status"`
 	Message string            `json:"message"`
 	Data    []ScheduleDetails `json:"data"`
+}
+
+type ScheduleDetails struct {
+	ID         int       `json:"id"`
+	Date       time.Time `json:"date"`
+	CinemaName string    `json:"cinema_name"`
+	ImagePath  string    `json:"image_path"`
+	Location   string    `json:"location"`
+	Time       string    `json:"time"`
+	MovieID    int       `json:"movie_id"`
 }
